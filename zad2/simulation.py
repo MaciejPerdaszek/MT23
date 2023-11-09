fa70b939ed9c300a4e5df7d8a776ebf787d671a6
MaciejPerdaszek <maciej.44@wp.pl>
2023-11-09 19:05:46 +0100

    Fix

--- zad2/simulation.py
+++ zad2/simulation.py
@@ -11,14 +11,15 @@ def function1(x):
         return 0
 
 
-# print(function1(100.008881))
+def function2(x):
+    return x * math.sin(10 * math.pi * x) + 1
 
 
-def simulatedAnnealing(x_start, T, dT, k, M):
+def simulatedAnnealing(x_start, T, dT, k, M, function, lower_range, upper_range):
     # stop condition?
     for j in range(M):
-        x_random = random.uniform(-150, 150)
-        dx = function1(x_random) - function1(x_start)
+        x_random = random.uniform(lower_range, upper_range)
+        dx = function(x_random) - function(x_start)
         if dx < 0:
             x_start = x_random
         else:
@@ -26,7 +27,10 @@ def simulatedAnnealing(x_start, T, dT, k, M):
             if x_new < math.exp(-dx / (k * T)):
                 x_start = x_random
         T = dT * T
-    print("x: " + str(x_start) + "\ny: " + str(function1(x_start)))
+    print("x: " + str(x_start) + "\ny: " + str(function(x_start)))
 
 
-simulatedAnnealing(0, 500, 0.999, 0.1, 3000)
+print("Function1")
+simulatedAnnealing(0, 500, 0.999, 0.1, 3000, function1, -150, 150)
+print("Function2")
+simulatedAnnealing(0, 500, 0.999, 0.1, 3000, function2, -1, 2)
