02390c50e866ee19125f3644c9bb4f45083637d4
Igor Bobrukiewicz <igor.bobrukiewicz@gmail.com>
2024-01-02 16:37:37 +0100

    Zadanie 5

--- zad5/functions.py
+++ zad5/functions.py
@@ -5,7 +5,7 @@ def get_function(i):
     if i == 1:
         return {'func': function1, 'x_min': -1.5, 'x_max': 4, 'y_min': -3, 'y_max': 4}
     elif i == 2:
-        return function2
+        return {'func': function2, 'x_min': -10, 'x_max': 10, 'y_min': -10, 'y_max': 10}
     else:
         return None
 
@@ -15,4 +15,4 @@ def function1(x, y):
 
 
 def function2(x, y):
-    return 100 * (y - x ** 2) ** 2 + (1 - x) ** 2
+    return (math.sin(3*math.pi*x)**2) + ((x - 1)**2) * (1 + (math.sin(3*math.pi*y)**2)) + ((y - 1)**2) * (1 + (math.sin(2*math.pi*y)**2))
